6c55881e3d9fba14713abcd3c55f01af6314cb59
Rinalds Pudulis <rpudulis@gmail.com>
2017-11-28 16:27:21 +0200

    refactor: Preparing project boilerplate structure

---
+++ client/src/components/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Main extends Component {
+    render() {
+        return (
+            <h1>Readable Project</h1>
+        )
+    }
+}
+
+export default Main
